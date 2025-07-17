27ca327a8bb60a58b9a358dccd86b38d60cd07c4
DavideCriscuolo <davide.alcatel@gmail.com>
2025-07-17 18:00:44 +0200

    fixato use state

--- src/Components/AppMain.jsx
+++ src/Components/AppMain.jsx
@@ -1,18 +1,40 @@
-import Button from "./Button";
+import { useState } from "react";
+import Card from "./Card";
 import { lenguages } from "../data/ListLenguages";
 export default function AppMain() {
+  // let leanguage = {}
+  const [leanguage, setLeanguage] = useState({
+    id: null,
+    title: "-",
+    desc: "Seleziona un linguaggio",
+  });
+
+  function heandClik(index) {
+    console.log(index);
+    setLeanguage(index);
+  }
+
   return (
     <main>
-      <div className="container d-flex justify-content-center  gap-5">
-        {lenguages.map((leanguage) => {
+      <div className="container d-flex justify-content-around ">
+        {lenguages.map((leanguage, index) => {
           return (
-            <Button
-              key={leanguage.id}
-              title={leanguage.title}
-              desc={leanguage.desc}
-            ></Button>
+            <div>
+              <div key={leanguage.id}>
+                <button
+                  className="btn btn-primary my-5"
+                  onClick={() => heandClik(leanguage)}
+                >
+                  {leanguage.title}
+                </button>
+              </div>
+            </div>
           );
         })}
+      </div>
+
+      <div>
+        <Card title={leanguage.title} desc={leanguage.desc}></Card>
       </div>
     </main>
   );
